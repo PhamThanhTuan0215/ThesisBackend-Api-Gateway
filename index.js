48abd22a4244d340f4a25ae647815f464b11b6ec
PhamThanhTuan0215 <thanhtuancr1234@gmail.com>
2025-06-29 10:58:13 +0700

    update develop

--- index.js
+++ index.js
@@ -15,21 +15,21 @@ const {
     URL_ORDER_SERVICE,
     URL_PAYMENT_SERVICE,
     URL_SHIPMENT_SERVICE
-    } = process.env
+} = process.env
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }));
 
 app.get('/', (req, res) => {
     return res.status(200).json({ message: 'run api-gateway successfully' })
 })
-app.use('/product', proxy(URL_PRODUCT_SERVICE))
+app.use('/product', proxy(URL_PRODUCT_SERVICE, { limit: '100mb' }))
 app.use('/user', proxy(URL_USER_SERVICE))
 app.use('/customer', proxy(URL_CUSTOMER_SERVICE))
-app.use('/store', proxy(URL_STORE_SERVICE))
+app.use('/store', proxy(URL_STORE_SERVICE, { limit: '50mb' }))
 app.use('/discount', proxy(URL_DISCOUNT_SERVICE))
-app.use('/order', proxy(URL_ORDER_SERVICE))
+app.use('/order', proxy(URL_ORDER_SERVICE, { limit: '100mb' }))
 app.use('/payment', proxy(URL_PAYMENT_SERVICE))
 app.use('/shipment', proxy(URL_SHIPMENT_SERVICE))
 
